7795e3d76eabe46955489500c6d3d64b453747b6
chiagozie pio <nduagubapio@gmail.com>
2023-12-04 05:10:24 -0800

    finish the home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,18 @@
 import '../ComponentCss/Home.css'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <section className='home'>
-      <h1>home</h1>
+      <div className="action">
+        <p className='action-text'>A journey of a thousand miles starts with a step but will definitely feel better if that that step is taken in a nice comfortable and durable Shoe <br /><span>Shop Castle</span> boosts of five of efficiently making sure you are addressed as a nobleman because as they say.... <br /> <label  className='action-text_quote'> <span>"</span>you are addressed the way you dress<span>"</span></label></p>
+        
+        <button className="action-btn"><Link to ="/products" className='btn-link'>Shop now</Link></button>
+
+      </div>
+        <div className="action-img">
+          <img src="img/shoe_hero.jpg" alt="shoe_img" />
+        </div>
     </section>
   )
 }
